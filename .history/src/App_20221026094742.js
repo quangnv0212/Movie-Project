db6aeda051547c349f8c6f769b37670d414b8ab8
quangnv.0212 <quangnv.0212@gmail.com>
2022-10-26 10:03:05 +0700

    firstCommit

---
+++ .history/src/App_20221026094742.js
@@ -0,0 +1,38 @@
+import MoviePage from "pages/MoviePage";
+import { Fragment, lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import "swiper/scss";
+import Banner from "./components/banner/Banner";
+import Main from "./components/layout/Main";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage"));
+
+function App() {
+  return (
+    <Fragment>
+      <Suspense fallback={<></>}>
+        <Routes>
+          <Route element={<Main></Main>}>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Banner></Banner>
+                  <HomePage></HomePage>
+                </>
+              }
+            ></Route>
+            <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
+            <Route
+              path="/movie/:movieId"
+              element={<MovieDetailsPage></MovieDetailsPage>}
+            ></Route>
+          </Route>
+        </Routes>
+      </Suspense>
+    </Fragment>
+  );
+}
+
+export default App;
